3224cbb5e12b6a450f513a49d107b14a8e740f7e
Joaquin <jpgonzalezra@gmail.com>
2020-06-14 13:16:37 -0300

    feat: ropsten migration and deploy

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const MNEMONIC = "digital unknown jealous mother legal hedgehog save glory december universe spread figure custom found six"
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +8,12 @@ module.exports = {
       port: 7545,
       network_id: '*', // eslint-disable-line camelcase
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/c3422181d0594697a38defe7706a1e5b")
+      },
+      network_id: 3
+    }
   },
   compilers: {
     solc: {

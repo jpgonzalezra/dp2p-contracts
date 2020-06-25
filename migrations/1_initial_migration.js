3011f1329598e900a6c1a294acdb43e06fa04234
Shuffledex <shuffledex@gmail.com>
2020-06-24 22:54:59 -0300

    add new fake tokens to test purpouse

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,12 +1,15 @@
 const TestToken = artifacts.require("TestToken");
+const TestToken2 = artifacts.require("TestToken2");
+const TestToken3 = artifacts.require("TestToken3");
 const Stablescrow = artifacts.require("Stablescrow");
 
-module.exports = function(deployer) {
-    // Deployer is the Truffle wrapper for deploying
-    // contracts to the network
+module.exports = function (deployer) {
+  // Deployer is the Truffle wrapper for deploying
+  // contracts to the network
 
-    // Deploy the contract to the network
-    deployer.deploy(TestToken);
-    deployer.deploy(Stablescrow);
-
-}
+  // Deploy the contract to the network
+  deployer.deploy(TestToken);
+  deployer.deploy(Stablescrow);
+  deployer.deploy(TestToken2);
+  deployer.deploy(TestToken3);
+};

7e0ecbb77421f5e32f8e66cff7fc483abbf2ffb8
Joaquin <jpgonzalezra@gmail.com>
2020-07-04 21:24:05 -0300

    chore: new test token (8 digits)

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,5 @@
-const TestToken = artifacts.require("TestToken");
+const TestToken = artifacts.require("TestToken"); // 18 digits
+const TestToken2 = artifacts.require("TestToken2"); // 8 digits
 const Stablescrow = artifacts.require("Stablescrow");
 
 module.exports = function(deployer) {
@@ -7,6 +8,7 @@ module.exports = function(deployer) {
 
     // Deploy the contract to the network
     deployer.deploy(TestToken);
+    deployer.deploy(TestToken2);
     deployer.deploy(Stablescrow);
 
 }

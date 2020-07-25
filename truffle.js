cd3f62d9d4c2d4f388a87683c932ee9a26d2e80c
Joaquin <jpgonzalezra@gmail.com>
2020-07-25 16:48:39 -0300

    chore: updated truffle config

--- truffle.js
+++ truffle.js
@@ -9,14 +9,15 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/c3422181d0594697a38defe7706a1e5b")
+        return new HDWalletProvider("0x6365b6515d20004503c3d6e87226880c9903f17283d9cd4ee4d02c90b3f2b5b0", "https://ropsten.infura.io/v3/f039330d8fb747e48a7ce98f51400d65")
       },
+      gasPrice: 40000000000,
       network_id: 3
     }
   },
   compilers: {
     solc: {
-      version: '0.6.4',
+      version: '0.6.2',
       docker: false,
       settings: {
         optimizer: {

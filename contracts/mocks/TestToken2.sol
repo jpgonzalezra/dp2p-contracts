a18235141d0c1fcf0818afa89e43557157883447
Shuffledex <shuffledex@gmail.com>
2020-08-15 09:53:04 -0300

    change class name

--- contracts/mocks/TestToken2.sol
+++ contracts/mocks/TestToken2.sol
@@ -4,7 +4,7 @@ pragma solidity 0.6.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // mock class using ERC20
-contract TestToken1 is ERC20 {
+contract TestToken2 is ERC20 {
     constructor() public payable ERC20("Test2", "TEST2") {
         _setupDecimals(6);
     }

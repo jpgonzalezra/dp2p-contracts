529ea8f2f1873a4171258470762f8e8addaf2dc2
Shuffledex <shuffledex@gmail.com>
2020-08-23 23:17:06 -0300

    migrate tokens

--- contracts/mocks/TestToken2.sol
+++ contracts/mocks/TestToken2.sol
@@ -3,11 +3,10 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 // mock class using ERC20
 contract TestToken2 is ERC20 {
     constructor() public payable ERC20("Test2", "TEST2") {
-        _setupDecimals(6);
+        _setupDecimals(8);
     }
 
     function mint(address account, uint256 amount) public {
